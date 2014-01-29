7890ba0a780dfcfe39f0b98f313635f0ed8eec60
Bill Burke <billstron@gmail.com>
2014-01-29 06:43:31 -0500

    Added content from old site

---
+++ site_look.js
@@ -0,0 +1,15 @@
+function sideBar()
+{
+	document.write("<a href='index.html'>Home</a><br />");
+	document.write("<a href='aboutme.html'>About Me</a><br />");
+	document.write("<a href='research.html'>Research</a><br />");
+	document.write("<br />");
+	document.write("<a href='wiki/Main_Page'>Wiki</a><br />");
+	document.write("<a href='blog'>Blog</a><br />");
+	document.write("<a href='personal.html'>Personal</a><br />");
+	document.write("<a href='sandbox.html'>Sandbox</a><br />");
+	document.write("<a href='links.html'>Links</a><br />");
+	document.write("<br />");
+	//document.write("<a href='http://wjburke.com'>Professional</a><br />");
+	document.write("<a href='contact.html'>Contact</a><br/>");
+}
